f1553ace64f2e86551613f3e7bdbbda0e6ca3655
Vitor Zortéa <vitorzortea@gmail.com>
2020-10-09 10:44:43 -0300

    rotas e abas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { JobComponent } from './job/job.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SkillComponent } from './skill/skill.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'profile',
+  },
+  {
+    path: '',
+    children: [
+      {
+        path: 'profile',
+        component: ProfileComponent,
+      },
+      {
+        path: 'skill',
+        component: SkillComponent
+      },
+      {
+        path: 'job',
+        component: JobComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
